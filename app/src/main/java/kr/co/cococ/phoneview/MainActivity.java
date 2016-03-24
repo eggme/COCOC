3ee76cef70f51d166ef76b191b7c5571dc31a5ab
kyyeto9984 <kyyeto9984@naver.com>
2016-03-24 17:41:01 +0900

    안민쥐

--- app/src/main/java/kr/co/cococ/phoneview/MainActivity.java
+++ app/src/main/java/kr/co/cococ/phoneview/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 시발
+        // 안민지랄
+        // 장영우라늄
     }
 
     @Override
