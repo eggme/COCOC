ddab39127a4bacafb3ceb4654643edea06e7f576
kyyeto9984 <kyyeto9984@naver.com>
2016-03-27 17:58:07 +0900

    변경사항 추가

--- app/src/main/java/kr/co/cococ/phoneview/MainActivity.java
+++ app/src/main/java/kr/co/cococ/phoneview/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 안민지랄
-        // 장영우라늄
     }
 
     @Override
